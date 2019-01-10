b4ef491d62555d19eeb8d12196289773020996bc
yuming <536767875@qq.com>
2019-01-10 11:36:33 +0800

    lodash

---
+++ lodash/minititan12-lodash.js
@@ -0,0 +1,17 @@
+var minititan12 = function(){
+
+    function compact(ary){
+        var result = [];
+        var len = ary.length;
+        for(i = 0; i < len; i++){
+            if(ary[i]){
+                result.push(ary[i]);
+            }
+        }
+        return result;
+    }
+    return {
+        chunk:chunk,
+        compact:compact,
+    }
+}()
