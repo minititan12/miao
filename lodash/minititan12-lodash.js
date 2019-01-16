c3879a6e26dcec9e89310cd5255f0e6a4ba4bee6
yuming <536767875@qq.com>
2019-01-16 20:59:51 +0800

    l

--- lodash/minititan12-lodash.js
+++ lodash/minititan12-lodash.js
@@ -193,9 +193,11 @@ var minititan12 = function(){
             }
         }
 
-        for(let val in check){
-            if(check[val] >= arguments.length){
-                result.push(+val);
+        for(let i = 0; i < all.length; i++){
+            let val = all[i];
+            if(check[val] > 1){
+                result.push(val);
+                check[val] = 1;
             }
         }
 
@@ -236,26 +238,32 @@ var minititan12 = function(){
 
     //移除数组array中所有和给定值相等的元素，使用全等比较
     function pull(ary = [],...values){
-        let value = [].concat(values);
-        for(let i = 0; i < value.length; i++){
-            for(let j = 0; j < ary.length; j++){
-                if(ary[j] === value[i]){
-                    del(ary,j);
-                    j--;
-                }
-            }
-        }
-        return ary;
-        function del(ary, n){
-            for(let i = 0; i < ary.length; i++){
-                if(i >= n){
-                    ary[i] = ary[i + 1];
-                }
-            }
-            ary.pop();
-            return ary;
-        }
+        let value = [].concat(...values);
+        return ary.filter(function(item){
+            return value.indexOf(item) == -1;
+        })
     }
+    // function pull(ary = [],...values){
+    //     let value = [].concat(values);
+    //     for(let i = 0; i < value.length; i++){
+    //         for(let j = 0; j < ary.length; j++){
+    //             if(ary[j] === value[i]){
+    //                 del(ary,j);
+    //                 j--;
+    //             }
+    //         }
+    //     }
+    //     return ary;
+    //     function del(ary, n){
+    //         for(let i = 0; i < ary.length; i++){
+    //             if(i >= n){
+    //                 ary[i] = ary[i + 1];
+    //             }
+    //         }
+    //         ary.pop();
+    //         return ary;
+    //     }
+    // }
 
     //翻转数组
     function reverse(ary = []){
@@ -269,6 +277,159 @@ var minititan12 = function(){
         }
         return ary;
     }
+
+    //裁剪数组，从start开始到end结束，但不包括end本身的位置
+    function slice(ary = [],start = 0, end = ary.length){
+        let result = [];
+        for(let i = start; i < end; i++){
+            result.push(ary[i]);
+        }
+        return result;
+    }
+
+    //获取除了array数组第一个元素以外的全部元素
+    function tail(ary = []){
+        let result = [];
+        for(let i = 1; i < ary.length; i++){
+            result.push(ary[i]);
+        }
+        return result;
+    }
+
+    //创建一个数组切片,从array数组的起始元素开始提取n个元素
+    function take(ary = [],n = 1){
+        let result = [];
+        for(let i = 0; i < n; i++){
+            result.push(ary[i]);
+        }
+        return result;
+    }
+
+    //创建一个数组切片，从array数组的最后一个元素开始提取n个元素
+    function takeRight(ary = [],n = 1){
+        let result = [];
+        let len = ary.length;
+        if(n > len){
+            n = len;
+        }
+        for(let i = len - n; i < len; i++){
+            result.push(ary[i]);
+        }
+        return result;
+    }
+
+    //创建一个按顺序排列的唯一值的数组(所有给定数组的并集)
+    function union(...ary){
+        let all = [].concat(...ary);
+        let result = all.filter(function(item,index){
+            let check = all.indexOf(item);
+            return check == index ? true : false;
+        })
+        return result;
+    }
+    // function union(...ary){
+    //     let result = [];
+    //     let all = [].concat(...ary);
+    //     let check = {};
+
+    //     for(let i = 0; i < all.length; i++){
+    //         let val = all[i];
+    //         if(val in check){
+    //             check[val]++;
+    //         }else{
+    //             check[val] = 1;
+    //         }
+    //     }
+
+    //     for(let i = 0; i < all.length; i++){
+    //         let val = all[i];
+    //         if(check[val]>=1){
+    //             result.push(val);
+    //             check[val] = 0;
+    //         }
+    //     }
+    //     return result;
+    // }
+
+    //创建一个去重后的array数组副本
+    function uniq(ary = []){
+        let result = ary.filter(function(item,index){
+            let check = ary.indexOf(item);
+            return check == index ? true : false;
+        })
+        return result;
+    }
+    // function uniq(ary = []){
+    //     let len = ary.length;
+    //     let check = {};
+    //     let result = [];
+    //     for(let i = 0; i < len; i++){
+    //         let val = ary[i];
+    //         if(val in check){
+    //             check[val]++;
+    //         }else{
+    //             check[val] = 1;
+    //         }
+    //     }
+
+    //     for(let i = 0; i < len; i++){
+    //         let val = ary[i];
+    //         if(check[val] == 1){
+    //             result.push(val);
+    //         }else if(check[val] > 1){
+    //             result.push(val);
+    //             check[val] = 0;
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
+    //创建一个剔除所有给定值的新数组，剔除时用全等
+    function without(ary = [],...values){
+        let value = [].concat(...values);
+        let result = ary.filter(function(item){
+            return value.indexOf(item) == -1;
+        })
+        return result;
+    }
+
+    //创建一个给定数组唯一值的数组,返回值的顺序取决于他们数组的出现顺序
+    function xor(...ary){
+        let result = [];
+        let len = arguments.length;
+        for(let i = 0; i < len; i++){
+            result.push(arguments[i][i]);
+        }
+        return result;
+    }
+
+    //创建一个分组元素的数组，数组的第一个元素包含所有给定数组的第一个元素,依次类推
+    function zip(...ary){
+        let len = arguments.length;
+        let result = [];
+        let max = 0;
+
+        for(let i = 0; i < len; i++){
+            let l = arguments[i].length;
+            if(l > max){
+                max = l;
+            }
+        }
+
+        for(let i = 0; i < max; i++){
+            let pick = [];
+            for(let j = 0; j < len; j++){
+                let val = arguments[j][i];
+                if(val){
+                    pick.push(val);
+                }
+            }
+            result.push(pick);
+        }
+        return result;
+    }
+
 
     return {
         chunk: chunk,
@@ -290,5 +451,14 @@ var minititan12 = function(){
         nth: nth,
         pull: pull,
         reverse: reverse,
+        slice: slice,
+        tail: tail,
+        take: take,
+        takeRight: takeRight,
+        union: union,
+        uniq: uniq,
+        without: without,
+        xor: xor,
+        zip: zip,
     }
 }()
